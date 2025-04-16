f880e151fc7e297b2f3057d720a612ac980b012e
Hasan-Uz-Zaman <hasanashik100@gmail.com>
2025-04-16 12:52:50 +0600

    added second version

--- dog.go
+++ dog.go
@@ -1,9 +1,11 @@
 package dog
 
 import (
+	"fmt"
 	"strings"
 )
 
 func WhenGrownUp(s string) string {
+	fmt.Println("This is dog version 1.2.0")
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
